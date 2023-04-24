14df681466918563a71dec7041d529b689fc66ea
esdb1 <pierre.lacognata@mail-esd.com>
2023-04-24 15:03:30 +0200

    avec d'autres trucs

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(Request $request): Response
+    {
+
+        $prenom = 'toi';
+      if($request->get("prenom")){
+          $prenom = $request->get("prenom");
+      }
+
+
+
+
+        return $this->render('home/index.html.twig', [
+        'prenom'=>$prenom
+        ]);
+    }
+}
